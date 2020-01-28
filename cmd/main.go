c1db5da1747e3b87582524197c6007a34776f8c9
fguedes <luis.guedes@resultadosdigitais.com.br>
2020-01-28 17:37:39 -0300

    Fix identation

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-        "github.com/fernandoguedes/rgb-soccer"
+	"github.com/fernandoguedes/rgb-soccer"
 )
 
 const PORT = ":8081"
 
 func main() {
-        http.HandleFunc("/", rigabe.Rigabe)
-        fmt.Println("Listening on localhost:8080")
-        log.Fatal(http.ListenAndServe(PORT, nil))
+	http.HandleFunc("/", rigabe.Rigabe)
+	fmt.Println("Listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
